a6ec925c821aba830b5660eb3f46b7c8d27d71c6
Adriana Chiquito <144694058+adrianacp10@users.noreply.github.com>
2024-04-02 17:29:21 +0000

    final

--- src/models.py
+++ src/models.py
@@ -7,23 +7,54 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id_user = Column(Integer, primary_key=True)
+    username = Column(String(120), nullable=False)
+    firstname = Column(String(120), nullable=False)
+    lastname = Column(String(120), nullable=False)
+    email = Column(String(120), nullable=False)
+    password = Column(String(80), unique=False, nullable=False)
+
+
+class Characters(Base):
+    __tablename__ = 'characters'
+
+    id_characters = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('id.user'))
+    user = relationship(User)
+    gender = Column(String(25), nullable = False)
+    birth_year = Column(String(25), nullable = False)
+
+
+class Likes(Base):
+    __tablename__ = 'likes'
+
+    id_likes = Column(Integer, primary_key=True)
+    user_to_id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, nullable=False)
+    user = relationship(User)
+    
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+
+    id_vehicles = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, nullable=False)
+    post_id = Column(Integer, nullable=False)
+    user = relationship(User)
+    post = relationship(Post)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+
+    id_planets = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    name = Column(String(25), nullable = False)
+    population = Column(Integer, nullable=False)
+
+    
 
     def to_dict(self):
         return {}
